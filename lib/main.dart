736d940b4cfe76b72e63f20b49e09f6b9940e962
vinayanys <ainunissavinaya@gmail.com>
2021-07-09 00:40:17 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,325 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+    return new Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {},
+            color: Colors.black,
+          ),
+          title: Text('Fine quality', style: TextStyle(color: Colors.black)),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+              color: Colors.grey,
             ),
           ],
         ),
+        body: ListView(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  height: 250.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Stack(
+                            children: <Widget>[
+                              Container(
+                                height: 230.0,
+                                width: MediaQuery.of(context).size.width -
+                                    MediaQuery.of(context).size.width / 3,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                    image: DecorationImage(
+                                        image: AssetImage('assets/img4.jpg'),
+                                        fit: BoxFit.cover)),
+                              ),
+                              Positioned(
+                                left: 15.0,
+                                top: 130.0,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Honey Bread',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'Quicksand',
+                                          fontSize: 30.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '\$88',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Quicksand',
+                                        fontSize: 20.0,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(width: 5.0),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Material(
+                            borderRadius: BorderRadius.circular(7.0),
+                            elevation: 2.0,
+                            child: Container(
+                              height: 60.0,
+                              width: 60.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(7.0)),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Icon(Icons.favorite, color: Colors.red),
+                                  Text(
+                                    '368',
+                                    style: TextStyle(
+                                        fontFamily: 'Quicksand',
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Material(
+                            borderRadius: BorderRadius.circular(7.0),
+                            elevation: 2.0,
+                            child: Container(
+                              height: 60.0,
+                              width: 60.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7.0),
+                              ),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Icon(Icons.chat_bubble,
+                                      color: Colors.grey.withOpacity(0.5)),
+                                  Text(
+                                    '76',
+                                    style: TextStyle(
+                                        fontFamily: 'Quicksand',
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Material(
+                            borderRadius: BorderRadius.circular(7.0),
+                            elevation: 2.0,
+                            child: Container(
+                              height: 60.0,
+                              width: 60.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7.0),
+                              ),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Icon(Icons.arrow_forward, color: Colors.grey),
+                                  Text(
+                                    '18',
+                                    style: TextStyle(
+                                        fontFamily: 'Quicksand',
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 17.0),
+                  child: Text(
+                    'Commodity',
+                    style: TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                GridView.count(
+                  crossAxisCount: 2,
+                  primary: false,
+                  crossAxisSpacing: 10.0,
+                  childAspectRatio: 0.85,
+                  mainAxisSpacing: 10.0,
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    _buildFoodCard(
+                        'Raspberry', 'Italy', 'assets/img1.jpg', 64, 22, 1),
+                    _buildFoodCard(
+                        'Mousse', 'China', 'assets/img2.jpg', 64, 22, 2),
+                    _buildFoodCard(
+                        'Raspberry', 'Italy', 'assets/img3.jpg', 64, 22, 3),
+                    _buildFoodCard(
+                        'Cupcake', 'Sweden', 'assets/img5.jpg', 64, 22, 4),
+                    _buildFoodCard(
+                        'Cupcake', 'Sweden', 'assets/img6.jpg', 64, 22, 5),
+                    _buildFoodCard(
+                        'Cupcake', 'Sweden', 'assets/img7.jpg', 64, 22, 6),
+                  ],
+                ),
+                SizedBox(height: 10.0)
+              ],
+            )
+          ],
+        ));
+  }
+
+  Widget _buildFoodCard(String name, String origin, String cardImage, int likes,
+      int commentCount, int cardIndex) {
+    return Padding(
+      padding: cardIndex.isEven
+          ? EdgeInsets.only(right: 15.0)
+          : EdgeInsets.only(left: 15.0),
+      child: Container(
+        height: 400.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(
+                color: Colors.grey.withOpacity(0.2),
+                style: BorderStyle.solid,
+                width: 1.0)),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 125.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          topRight: Radius.circular(10.0)),
+                      image: DecorationImage(
+                          image: AssetImage(cardImage), fit: BoxFit.cover)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0, top: 5.0),
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      fontFamily: 'Quicksand',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(
+                    origin,
+                    style: TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontSize: 12.0,
+                        color: Colors.grey),
+                  ),
+                ),
+                SizedBox(height: 3.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5.0, top: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.grey.withOpacity(0.4),
+                      ),
+                      SizedBox(width: 2.0),
+                      Text(
+                        likes.toString(),
+                        style: TextStyle(
+                            fontFamily: 'Quicksand',
+                            fontSize: 12.0,
+                            color: Colors.grey),
+                      ),
+                      SizedBox(width: 10.0),
+                      Icon(
+                        Icons.chat_bubble,
+                        color: Colors.grey.withOpacity(0.4),
+                      ),
+                      SizedBox(width: 2.0),
+                      Text(
+                        commentCount.toString(),
+                        style: TextStyle(
+                            fontFamily: 'Quicksand',
+                            fontSize: 12.0,
+                            color: Colors.grey),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Positioned(
+              left: 110.0,
+              top: 102.0,
+              child: Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.amber),
+                child: Center(
+                  child: Icon(Icons.shopping_cart, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
